07a95a5933f2cfd8b2bb62a3389cf8552981e8fc
Petar Vujović <petarvujovic98@gmail.com>
2022-05-18 11:49:45 +0200

    Move MutationObserver creation inside useEffect

--- src/hooks/useIsDarkMode.ts
+++ src/hooks/useIsDarkMode.ts
@@ -6,24 +6,24 @@ import { useEffect, useState } from 'react';
 export function useIsDarkMode() {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
-  // Create a mutation observer to set the dark mode state when the 'data-theme' attribute
-  // changes
-  const observer = new MutationObserver((mutations) => {
-    for (const mutation of mutations) {
-      if (
-        mutation.type === 'attributes' &&
-        mutation.attributeName === 'data-theme'
-      ) {
-        setIsDarkMode(
-          (mutation.target as Element).getAttribute('data-theme') === 'dark',
-        );
-      }
-    }
-  });
-
   // Start observing the 'data-theme' attribute on the root html element
   // Stop the observer when the component using the hook unmounts
   useEffect(() => {
+    // Create a mutation observer to set the dark mode state when the
+    //'data-theme' attribute changes
+    const observer = new MutationObserver((mutations) => {
+      for (const mutation of mutations) {
+        if (
+          mutation.type === 'attributes' &&
+          mutation.attributeName === 'data-theme'
+        ) {
+          setIsDarkMode(
+            (mutation.target as Element).getAttribute('data-theme') === 'dark',
+          );
+        }
+      }
+    });
+
     observer.observe(document.querySelector('html'), { attributes: true });
 
     return () => {
